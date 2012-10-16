195c33fa0d45caae5f4c5fe27631639fe7d05e0d
Mathew McBride <matt@bionicmessage.net>
2012-10-16 20:12:21 +1100

    RasPi GPIO activated for 5 seconds at a time

--- actions-raspi.c
+++ actions-raspi.c
@@ -6,23 +6,29 @@
 #include <bcm2835.h>
 
 #define PIN_FAIL RPI_GPIO_P1_12
-#define PIN_SUCCESS RPI_GPIO_P1_16
+#define PIN_LED_SUCCESS RPI_GPIO_P1_16
+#define PIN_DOOR_SUCCESS RPI_GPIO_P1_22
 int connect_to_serial() {
 	if (!bcm2835_init()) {
 		return 1;
 	}
 	
-	bcm2835_gpio_fsel(PIN_SUCCESS, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(PIN_LED_SUCCESS, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(PIN_DOOR_SUCCESS, BCM2835_GPIO_FSEL_OUTP);
 	bcm2835_gpio_fsel(PIN_FAIL,BCM2835_GPIO_FSEL_OUTP);
 }
 
 void has_valid_card() {
-	bcm2835_gpio_write(PIN_SUCCESS, HIGH);
-	delay(500);
-	bcm2835_gpio_write(PIN_SUCCESS,LOW);
+	bcm2835_gpio_write(PIN_LED_SUCCESS, HIGH);
+	bcm2835_gpio_write(PIN_DOOR_SUCCESS,HIGH);
+	delay(5000);
+	delay(5000);
+	bcm2835_gpio_write(PIN_LED_SUCCESS,LOW);
+	bcm2835_gpio_write(PIN_DOOR_SUCCESS,LOW);
 }
 void has_invalid_card() {
 	bcm2835_gpio_write(PIN_FAIL, HIGH);
 	delay(500);
 	bcm2835_gpio_write(PIN_FAIL,LOW);
 }
+
