7ba5c22b98cc51d49717b350c59144dc61dda4cd
Mathew McBride <matt@bionicmessage.net>
2015-01-31 13:04:30 +0000

    Fix an issue with card formatting - formatter was expecting an INVALID rather than NOTFOUND for fresh cards

--- key-cutter/cutter-gui.c
+++ key-cutter/cutter-gui.c
@@ -73,7 +73,7 @@ int main(int argc, char **argv) {
                     } else if (status == CARDACTION_BLOCKED) {
                         drawBlockedCardScreen(uid);
                         sleep(5);
-                    } else if (status == CARDACTION_INVALID) {
+                    } else if (status == CARDACTION_INVALID || status == CARDACTION_NOTFOUND) {
                         drawFormattingScreen();
                         char b64KeyA[32],b64KeyB[32];
                         int format_status = format_card(tags[i],uid,&b64KeyA[0],&b64KeyB[0]);
